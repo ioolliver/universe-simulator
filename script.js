9f93a2ee5bb23533ca173600c247ebd0fd37f845
ionascimento <ioliveirasp45@gmail.com>
2022-05-23 10:11:25 -0300

    cursor interactive

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const context = canvas.getContext("2d", null);
 
 const AU = 149.6e6 * 1000; // 1 AU =~ 150 BILLIONS KM; * 1000 = CONVERTING KM TO METERS
 const G = 6.67428e-11;
-const SCALE = 300 / AU; // 1 AU = 100 pixels
+var SCALE = 150 / AU; // 1 AU = 100 pixels
 var TIMESTEP = 3600 * 24 * 1; // 1 day
 
 class Star {
@@ -20,7 +20,8 @@ class Star {
     this.name = name;
     this.x = x || 0;
     this.y = y || 0;
-    this.radius = radius;
+    this.radiusInfo = radius;
+    this.radius = radius * (SCALE*2000000);
     this.color = color;
 
     this.massInfo = massInfo;
@@ -43,15 +44,15 @@ class Star {
     context.beginPath();
     for(let point of this.orbit) {
 
-      context.lineTo(point[0] * SCALE + WIDTH/2, point[1] * SCALE + HEIGHT/2);
+      context.lineTo(point[0] * SCALE + WIDTH/2 + xVariant, point[1] * SCALE + HEIGHT/2 + yVariant);
 
     }
     context.strokeStyle = "#fff";
     context.stroke();
     context.closePath();
 
-    let x = this.x * SCALE + WIDTH/2;
-    let y = this.y * SCALE + HEIGHT/2;
+    let x = this.x * SCALE + WIDTH/2 + xVariant;
+    let y = this.y * SCALE + HEIGHT/2 + yVariant;
 
     context.beginPath();
     context.arc(x, y, this.radius, 0, 2 * Math.PI);
@@ -60,6 +61,7 @@ class Star {
     context.closePath();
 
     context.font = this.radius * 0.8 + "px Arial";
+    context.fillStyle = "#fff";
     context.fillText(this.name, x - this.radius * 0.6, y - this.radius*1.3);
 
   }
@@ -78,6 +80,12 @@ class Star {
     const forceY = Math.sin(theta) * force;
 
     return [forceX, forceY]
+
+  }
+
+  updateRadius() {
+
+    this.radius = this.radiusInfo * (SCALE*2000000);
 
   }
 
@@ -125,7 +133,7 @@ function main() {
 
   new Star({
     name: 'Sun',
-    radius: 40,
+    radius: 20000,
     color: "#ff0",
     massInfo: [1.989, 30]
   });
@@ -133,7 +141,7 @@ function main() {
   new Star({
     name: 'Mercury',
     x: 0.387*AU,
-    radius: 7,
+    radius: 2440,
     color: "#e5e5e5",
     massInfo: [3.30, 23],
     y_vel: -47.4 * 1000
@@ -142,7 +150,7 @@ function main() {
   new Star({
     name: 'Venus',
     x: 0.723*AU,
-    radius: 14,
+    radius: 6051,
     color: "#a57c1b",
     massInfo: [4.8685, 24],
     y_vel: -35.02 * 1000
@@ -151,10 +159,55 @@ function main() {
   new Star({
     name: 'Earth',
     x: -1*AU,
-    radius: 16,
+    radius: 6371,
     color: "#6b93d6",
     massInfo: [5.9742, 24],
     y_vel: 29.783 * 1000
+  });
+
+  new Star({
+    name: 'Mars',
+    x: -1.52*AU,
+    radius: 3390,
+    color: "#451804",
+    massInfo: [6.39, 23],
+    y_vel: 24.07 * 1000
+  });
+
+  new Star({
+    name: 'Jupiter',
+    x: -5.2*AU,
+    radius: 69911/3,
+    color: "#6b93d6",
+    massInfo: [1.898, 27],
+    y_vel: 13.06 * 1000
+  });
+
+  new Star({
+    name: 'Saturn',
+    x: -9.5*AU,
+    radius: 58232/3,
+    color: "#ab604a",
+    massInfo: [5.683, 26],
+    y_vel: 9.68 * 1000
+  });
+
+  new Star({
+    name: 'Uranus',
+    x: -19.8*AU,
+    radius: 25362/3,
+    color: "#e1eeee",
+    massInfo: [8.681, 25],
+    y_vel: 6.80 * 1000
+  });
+
+  new Star({
+    name: 'Neptune',
+    x: -30*AU,
+    radius: 24622/3,
+    color: "#5b5ddf",
+    massInfo: [10.024, 26],
+    y_vel: 5.43 * 1000
   });
 
   updateMassInfo();
@@ -228,5 +281,85 @@ document.querySelector('#dps').addEventListener('change', (e) => {
   const newValue = document.querySelector('#dps').value;
 
   TIMESTEP = 3600 * 24 * newValue;
+
+});
+
+document.querySelector('#scale').addEventListener('change', (e) => {
+  
+  const newValue = document.querySelector('#scale').value;
+
+  SCALE = newValue / AU;
+
+  for(let planet of planets) planet.updateRadius();
+
+});
+
+function getMousePos(evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left - WIDTH/2,
+    y: evt.clientY - rect.top - HEIGHT/2
+  };
+}
+
+var mouseDown = false;
+
+canvas.addEventListener('mousedown', (e) => {
+  mouseDown = true;
+});
+
+canvas.addEventListener('mouseup', (e) => { mouseDown = false; });
+
+var lastPosition = {x: 0, y: 0};
+
+var xVariant = 0;
+var yVariant = 0;
+
+canvas.addEventListener('mousemove', (e) => {
+
+  let mousePos = getMousePos(e);
+
+  if(mouseDown) {
+
+    if(mousePos.x != lastPosition.x) {
+      xVariant += mousePos.x - lastPosition.x;
+    }
+    if(mousePos.y != lastPosition.y) {
+      yVariant += mousePos.y - lastPosition.y;
+    }
+
+  }
+
+  lastPosition = mousePos;
+
+});
+
+canvas.addEventListener('wheel', (e) => {
+
+  e.preventDefault();
+
+  if(e.deltaY > 0) { // down / zoom-
+
+    let newValue = Number(document.querySelector('#scale').value) - 10;
+
+    if(newValue <= 0) newValue = 1;
+
+    document.querySelector('#scale').value = newValue;
+
+    SCALE = newValue / AU;
+
+    for(let planet of planets) planet.updateRadius();
+
+  }else {
+
+    let newValue = Number(document.querySelector('#scale').value) + 10;
+
+    document.querySelector('#scale').value = newValue;
+
+    SCALE = newValue / AU;
+
+    for(let planet of planets) planet.updateRadius();
+
+  }
 
 });
